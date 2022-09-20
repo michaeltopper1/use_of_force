d4a9f8627b8bbc068eb87a19c5e8f15bdb9c7e5c
Toshio Ferrazares <ferrazares@ucsb.edu>
2022-09-20 13:34:33 -0700

    adding analysis file for chicago (work in progress) and adding created data from clean_shifts and clean_trr

--- code/cleaning_chicago/merge_shifts_trr.r
+++
@@ -1,9 +0,0 @@
-#Script to merge shift data (all_shifts) and force data (trr_reports) 
-#   Toshio Ferrazares
-
-library(dplyr)
-library(tidyverse)
-library(janitor)
-library(readxl)
-library(fixest)
-library(lubridate)
